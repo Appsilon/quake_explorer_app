ccee91480232d359572b850cdb3ac36bd18706bb
Can Taşlıçukur <can.taslicukur@ozu.edu.tr>
2023-03-10 14:39:33 +0300

    linted app/view/mapQuake.R

--- app/view/mapQuake.R
+++ app/view/mapQuake.R
@@ -2,37 +2,39 @@
 
 box::use(
   shiny[moduleServer, NS, observe, req, observeEvent],
-  leaflet[renderLeaflet, leaflet, addTiles, setView, 
-          leafletProxy, clearControls, clearMarkers, addCircleMarkers,
-          addLegend, flyTo]
+  leaflet[
+    renderLeaflet, leaflet, addTiles, setView,
+    leafletProxy, clearControls, clearMarkers, addCircleMarkers,
+    addLegend, flyTo
+  ]
 )
 
 #' @export
 ui <- function(id) {
   ns <- NS(id)
-  
-  leafletOutput(ns('map'), height = "100%")
+
+  leafletOutput(ns("map"), height = "100%")
 }
 
 #' @export
 server <- function(id, data1, data2) {
   moduleServer(id, function(input, output, session) {
-    
     output$map <- renderLeaflet({
       leaflet() |>
         addTiles() |>
         setView(-27.210814, 30.161823, zoom = 2)
     })
-    
+
     observe({
       req(data1)
-      
+
       leafletProxy("map", data = data1()) |>
         clearControls() |>
         clearMarkers() |>
         addCircleMarkers(
-          radius = input$map_zoom * 2, popup = ~popup, color = ~map_points_palette(mag),
-          stroke = TRUE, lat = ~latitude, lng = ~longitude) %>%
+          radius = input$map_zoom * 2, popup = ~popup, color = ~ map_points_palette(mag),
+          stroke = TRUE, lat = ~latitude, lng = ~longitude
+        ) %>%
         addLegend(
           "bottomright",
           title = "Magnitude",
@@ -40,17 +42,15 @@ server <- function(id, data1, data2) {
           values = c(1, 3, 5, 7)
         )
     })
-    
+
     observe({
-      leafletProxy('map') |>
-        flyTo(lng = data2()[['lng']], lat = data2()[['lat']], zoom = 6)
+      leafletProxy("map") |>
+        flyTo(lng = data2()[["lng"]], lat = data2()[["lat"]], zoom = 6)
     })
-    
+
     observeEvent(input$zoom_out, {
-      leafletProxy('map') |>
+      leafletProxy("map") |>
         flyTo(-27.210814, 30.161823, zoom = 2)
     })
-    
-    
   })
 }
