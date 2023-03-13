a8f972705a72ac0e062475b9553eec4958e07ded
Pedro Silva <pedro@appsilon.com>
2023-03-13 13:00:54 +0100

    fix: Additional arguments.

--- app/logic/dataManipulation.R
+++ app/logic/dataManipulation.R
@@ -4,45 +4,45 @@ box::use(
   readr[read_csv],
   dplyr[distinct, mutate, rename, filter, arrange, select],
   stringr[str_to_title],
+  app/logic/make_popup[make_popup],
+  app/logic/display_quake[display_quake],
+  utils[...],
   purrr[pmap]
 )
 
 #' @export
-quake_data_read <- function(file){
+quake_data_read <- function(file) {
   read_csv(file)|>
   mutate(popup = make_popup(place, time, mag, depth))
 }
 
-
-
 #' @export
-quake_types_func <- function(data){
+quake_types_func <- function(data) {
   data |>
   distinct(type) |>
   mutate(text = str_to_title(type)) |>
   rename(key = type)
 }
 
-
 #' @export
-quake_filter_func <- function(data){
+quake_filter_func <- function(data, type, mag) {
   data |>
-    filter(type == input$type, mag >= input$mag)
+    filter(type == !!type, mag >= !!mag)
 }
 
 #' @export
-top_quakes_func <- function(data){
+top_quakes_func <- function(data, n_quakes) {
   data() |>
   arrange(desc(mag)) |>
-  head(input$n_quakes) |>
+  head(n_quakes) |>
   select(mag, place, time, depth, id) |>
   pmap(display_quake)
 }
 
 #' @export
-selected_quake_func <- function(data){
-  quake_index <- which(data()[['id']] == input$quake_id)
-  
+selected_quake_func <- function(data, quake_id) {
+  quake_index <- which(data()[['id']] == quake_id)
+
   list(
     lat = data()[['latitude']][quake_index],
     lng = data()[['longitude']][quake_index]
