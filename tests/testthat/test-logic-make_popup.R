07267338a486bbe397e5aaf53e0445ce109708eb
ineelhere <>
2023-03-14 17:13:58 +0530

    [ADD] unit test for app/logic/make_popup module

---
+++ tests/testthat/test-logic-make_popup.R
@@ -0,0 +1,30 @@
+box::use(
+  shiny[...],
+  glue[glue],
+  stringr[str_to_title],
+  testthat[...]
+)
+
+box::use(
+  app/logic/make_popup[...]
+)
+
+test_that("variables have a value", {
+  # set values for the input variables
+  place <- "Hogwarts"
+  time <- "2023-03-14 17:05:00"
+  mag <- 5.0
+  depth <- 10
+  
+  # call the function with the input values
+  output <- make_popup(place, time, mag, depth)
+  
+  # styles might change in future, so lets check if the values are called correctly or not
+  # check that the input variables are not null
+  expect_true(!is.null(place))
+  expect_true(!is.null(time))
+  expect_true(!is.null(mag))
+  expect_true(!is.null(depth))
+})
+
+# check with rhino::test_r()
